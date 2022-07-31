53855f56e925686484af7f06c551f29c40645507
brett ohland <brett@ohland.ca>
2022-07-31 11:44:20 -0600

    Updating URL parsing

--- [2022-06-14] ParseableFormatStyle/ParseableFormatStyle and You.playground/Pages/URL (iOS 16).xcplaygroundpage/Contents.swift
+++ [2022-06-14] ParseableFormatStyle/ParseableFormatStyle and You.playground/Pages/URL (iOS 16).xcplaygroundpage/Contents.swift
@@ -4,11 +4,36 @@ import Foundation
 
 if #available(iOS 16.0, *) {
 
-    try? URL.FormatStyle()
-        .parseStrategy.parse("https://jAppleseed:Test1234@apple.com:80/macbook-pro?get-free#someFragmentOfSomething")
+    do {
+        try URL.FormatStyle.Strategy(port: .defaultValue(80)).parse("http://www.apple.com") // http://www.apple.com:80
+        try URL.FormatStyle.Strategy(port: .optional).parse("http://www.apple.com") // http://www.apple.com
+        try? URL.FormatStyle.Strategy(port: .required).parse("http://www.apple.com") // nil
 
-    try? URL(
-        "https://jAppleseed:Test1234@apple.com:80/macbook-pro?get-free#someFragmentOfSomething",
-        strategy: URL.FormatStyle().parseStrategy
-    )
+        // This returns a valid URL
+        try URL.FormatStyle.Strategy()
+            .scheme(.required)
+            .user(.required)
+            .password(.required)
+            .host(.required)
+            .port(.required)
+            .path(.required)
+            .query(.required)
+            .fragment(.required)
+            .parse("https://jAppleseed:Test1234@apple.com:80/macbook-pro?get-free#someFragmentOfSomething")
+
+        // This throws an error
+        try URL.FormatStyle.Strategy()
+            .scheme(.required)
+            .user(.required)
+            .password(.required)
+            .host(.required)
+            .port(.required)
+            .path(.required)
+            .query(.required)
+            .fragment(.required)
+            .parse("https://jAppleseed:Test1234@apple.com/macbook-pro?get-free#someFragmentOfSomething")
+
+    } catch {
+        print(error)
+    }
 }
